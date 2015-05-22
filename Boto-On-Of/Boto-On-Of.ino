a54c895dbe28026516dda8ea5bf4aa2a9f6dcb32
Octavi Masià <octavi@octavimasia.com>
2015-05-22 11:58:55 +0200

    Códigos para programar un robot básico

---
+++ Boto-On-Of/Boto-On-Of.ino
@@ -0,0 +1,32 @@
+int const boto = 12;
+
+int estatBoto = 0;
+int estatBotoAnterior = 0;
+
+boolean robotEnable = false;
+
+void setup() {
+  
+  pinMode(13, OUTPUT);
+  pinMode(boto, INPUT);
+
+}
+
+void loop() {
+  estatBoto = digitalRead(boto);
+  delay(1);
+  
+  if(estatBoto != estatBotoAnterior){
+      if(estatBoto == HIGH){
+          robotEnable = !robotEnable;  
+          
+      }
+  }
+  
+  if (robotEnable == true){
+    digitalWrite(13, HIGH);
+    }else{
+      digitalWrite(13, LOW);
+    }
+  estatBotoAnterior = estatBoto;
+}
---
+++ SeguiLinees-AgoraBot-V2/SeguiLinees-AgoraBot-V2.ino
@@ -0,0 +1,58 @@
+ #include <Servo.h>
+ Servo myservoIzq;
+ Servo myservoDer;
+ 
+  
+ void setup() {  
+   Serial.begin (9600);  
+   
+   myservoIzq.attach(9);
+   myservoDer.attach(10); 
+   
+   
+ }  
+   
+ void loop() {  
+   
+    //int valorIzq =  map(analogRead(A1), 40, 900, 135, 75);
+    //int valorDer =  map(analogRead(A0), 40, 800, 0, 60);
+    
+    int valorIzq =  analogRead(A1);
+    int valorDer =  analogRead(A0);
+    delay(500);
+     
+    myservoIzq.write(valorIzq);
+    myservoDer.write(valorDer);
+    
+    
+     Serial.print("Sensor llum Izq: "); 
+     Serial.print(valorIzq);
+     Serial.print(" Sensor llum Der: ");  
+     Serial.println(valorDer); 
+     
+     
+     
+  
+     
+   
+ } 
+
+ void avant(){
+    myservoIzq.write(255);
+    myservoDer.write(0);
+   }
+   
+ void girarDer(){
+    myservoIzq.write(255);
+    myservoDer.write(55);
+   }
+   
+  void girarIzq(){
+    myservoIzq.write(75);
+    myservoDer.write(0);
+   }
+ 
+ void parar(){
+    myservoIzq.write(67);
+    myservoDer.write(68);
+   }
